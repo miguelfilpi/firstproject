7d7abe3aa2dbdbfd182c1b0f1c63a59ac3c21da0
Miguel Filpi <miguelfilpi@hotmail.com>
2022-11-10 23:53:25 -0300

    adicionando os endpoints

---
+++ src/main/java/com/devsuperior/userdept/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.devsuperior.userdept.controller;
+
+import com.devsuperior.userdept.entities.User;
+import com.devsuperior.userdept.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping
+    public List<User> findAll(){
+        List<User> result = repository.findAll();
+        return result;
+    }
+}
